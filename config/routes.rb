eec9af69cd60030813e10b7d3aa9bb142da81a18
Lucas Bassand <lucas.bassand@hotmail.com>
2023-06-20 15:29:11 +0100

    Comment test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to:"app#home"
 
   get "/articles/:id", to: "app#show"
-  get "/articles/tags/:id", to: "tag#show"
+  get "/articles/tags/:id", to: "tag#show" # Commit 
 
 end
